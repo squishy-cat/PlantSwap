37e4d716efd9a4d21f28909c55db9e70e9616b60
Eve Kozina <eve.kozina@gmail.com>
2022-02-15 12:08:26 -0500

    Profile page final fix

--- client/src/components/UserProfile.jsx
+++ client/src/components/UserProfile.jsx
@@ -28,178 +28,177 @@ function UserProfile( {currentUser, loaded, filterUserPlants, filterListedPlants
         fetch(`/api/users/${searchId}`)
         .then((res) => res.json())
         .then((user) => setUser(user))
-    },[loaded === true]);
+    },[loaded, currentUser]);
     
-    if (loaded===false) {
-        return (
-            <div>
-                Loading...
-            </div>
-        )
 
-    } else if (currentUser.id != user.id) {
-        // setLoaded(true)
-        return (
-            <div className="UserProfile">
-            <Figure>
-                <Figure.Image
-                    className="w-25 p-3"
-                    id="UserImage"
-                    src={user.photo}
+    if (loaded===true) {
+        if (currentUser.id != user.id) {
+            return (
+                <div className="UserProfile">
+                <Figure>
+                    <Figure.Image
+                        className="w-25 p-3"
+                        id="UserImage"
+                        src={user.photo}
+                    />
+                    <h2>About {user.name}</h2>
+                    <Badge 
+                        bg="success"
+                        id="Badge"
+                    >
+                        My bio:
+                    </Badge> 
+                    <Badge
+                        bg="light"
+                        id="Badge"
+                        style={{color:"darkgreen"}}
+                    >
+                        {user.bio}
+                    </Badge>
+                    <br />
+                    <Badge 
+                        bg="success"
+                        id="Badge"
+                    >
+                        My address:
+                    </Badge> 
+                    <Badge
+                        bg="light"
+                        id="Badge"
+                        style={{color:"darkgreen"}}
+                    >
+                        {user.address}
+                    </Badge>
+                    <br />
+                    <Badge 
+                        bg="success"
+                        id="Badge"
+                    >
+                        My favorite plants:
+                    </Badge>
+                    <Badge
+                        bg="light"
+                        id="Badge"
+                        style={{color:"darkgreen"}}
+                    >
+                        {user.fav_plant}
+                    </Badge>
+                    <br />
+                    <Badge 
+                        bg="success"
+                        id="Badge"
+                    >
+                        I'm interested in:
+                    </Badge>
+                    <Badge
+                        bg="light"
+                        id="Badge"
+                        style={{color:"darkgreen"}}
+                    >
+                        {user.interested_in}
+                    </Badge>
+                </Figure>
+                <br />
+                <h2>{user.name}'s Listed Plants</h2>
+                <ViewPlants 
+                    allPlants={filterListedPlantsForUser(user.id)}
+                    currentUser={currentUser.id}
+                    profileUserId={user.id}
+                    filterListedPlantsForUser={filterListedPlantsForUser}
                 />
-                <h2>About {user.name}</h2>
-                <Badge 
-                    bg="success"
-                    id="Badge"
-                >
-                    My bio:
-                </Badge> 
-                <Badge
-                    bg="light"
-                    id="Badge"
-                    style={{color:"darkgreen"}}
-                >
-                    {user.bio}
-                </Badge>
-                <br />
-                <Badge 
-                    bg="success"
-                    id="Badge"
-                >
-                    My address:
-                </Badge> 
-                <Badge
-                    bg="light"
-                    id="Badge"
-                    style={{color:"darkgreen"}}
-                >
-                    {user.address}
-                </Badge>
-                <br />
-                <Badge 
-                    bg="success"
-                    id="Badge"
-                >
-                    My favorite plants:
-                </Badge>
-                <Badge
-                    bg="light"
-                    id="Badge"
-                    style={{color:"darkgreen"}}
-                >
-                    {user.fav_plant}
-                </Badge>
-                <br />
-                <Badge 
-                    bg="success"
-                    id="Badge"
-                >
-                    I'm interested in:
-                </Badge>
-                <Badge
-                    bg="light"
-                    id="Badge"
-                    style={{color:"darkgreen"}}
-                >
-                    {user.interested_in}
-                </Badge>
-            </Figure>
-            <br />
-            <h2>{user.name}'s Listed Plants</h2>
-            <ViewPlants 
-                allPlants={filterListedPlantsForUser(user.id)}
-                currentUser={currentUser.id}
-                profileUserId={user.id}
-                filterListedPlantsForUser={filterListedPlantsForUser}
-            />
-        <Outlet />
-        </div>
+                <Outlet />
+                </div>
+            )
+        } else if (currentUser.id==user.id) {
+            return (
+                <div className="UserProfile">
+                    <Figure>
+                        <Figure.Image
+                            className="w-25 p-3"
+                            id="UserImage"
+                            src={user.photo}
+                        />
+                        <h2>{user.name}</h2>
+                        <Badge 
+                            bg="success"
+                            id="Badge"
+                        >
+                            My bio:
+                        </Badge> 
+                        <Badge
+                            bg="light"
+                            id="Badge"
+                            style={{color:"darkgreen"}}
+                        >
+                            {user.bio}
+                        </Badge>
+                        <br />
+                        <Badge 
+                            bg="success"
+                            id="Badge"
+                        >
+                            My address:
+                        </Badge> 
+                        <Badge
+                            bg="light"
+                            id="Badge"
+                            style={{color:"darkgreen"}}
+                        >
+                            {user.address}
+                        </Badge>
+                        <br />
+                        <Badge 
+                            bg="success"
+                            id="Badge"
+                        >
+                            My favorite plants:
+                        </Badge>
+                        <Badge
+                            bg="light"
+                            id="Badge"
+                            style={{color:"darkgreen"}}
+                        >
+                            {user.fav_plant}
+                        </Badge>
+                        <br />
+                        <Badge 
+                            bg="success"
+                            id="Badge"
+                        >
+                            I'm interested in:
+                        </Badge>
+                        <Badge
+                            bg="light"
+                            id="Badge"
+                            style={{color:"darkgreen"}}
+                        >
+                            {user.interested_in}
+                        </Badge>
+                    </Figure>
+                    <br />
+                    <Button
+                        variant="outline-success"
+                        onClick={handleClick}
+                    >
+                        Edit my profile
+                    </Button>
+                    <br />
+                    <br />
+                    <h2>My Plants</h2>
+                    <ViewPlants 
+                        allPlants={filterUserPlants(user.id)}
+                        currentUser={currentUser.id}
+                        getPlants={getPlants}
+                    />
+                <Outlet />
+                </div>
+            )
+        } 
+    }else {
+         return (
+            <div>Loading...</div>
         )
-    } else if (currentUser.id==user.id)
-    return (
-        <div className="UserProfile">
-            {/* {setLoaded(true)} */}
-            <Figure>
-                <Figure.Image
-                    className="w-25 p-3"
-                    id="UserImage"
-                    src={user.photo}
-                />
-                <h2>{user.name}</h2>
-                <Badge 
-                    bg="success"
-                    id="Badge"
-                >
-                    My bio:
-                </Badge> 
-                <Badge
-                    bg="light"
-                    id="Badge"
-                    style={{color:"darkgreen"}}
-                >
-                    {user.bio}
-                </Badge>
-                <br />
-                <Badge 
-                    bg="success"
-                    id="Badge"
-                >
-                    My address:
-                </Badge> 
-                <Badge
-                    bg="light"
-                    id="Badge"
-                    style={{color:"darkgreen"}}
-                >
-                    {user.address}
-                </Badge>
-                <br />
-                <Badge 
-                    bg="success"
-                    id="Badge"
-                >
-                    My favorite plants:
-                </Badge>
-                <Badge
-                    bg="light"
-                    id="Badge"
-                    style={{color:"darkgreen"}}
-                >
-                    {user.fav_plant}
-                </Badge>
-                <br />
-                <Badge 
-                    bg="success"
-                    id="Badge"
-                >
-                    I'm interested in:
-                </Badge>
-                <Badge
-                    bg="light"
-                    id="Badge"
-                    style={{color:"darkgreen"}}
-                >
-                    {user.interested_in}
-                </Badge>
-            </Figure>
-            <br />
-            <Button
-                variant="outline-success"
-                onClick={handleClick}
-            >
-                Edit my profile
-            </Button>
-            <br />
-            <br />
-            <h2>My Plants</h2>
-            <ViewPlants 
-                allPlants={filterUserPlants(user.id)}
-                currentUser={currentUser.id}
-                getPlants={getPlants}
-            />
-        <Outlet />
-        </div>
-    )
+    }
 }
 
 export default UserProfile;
